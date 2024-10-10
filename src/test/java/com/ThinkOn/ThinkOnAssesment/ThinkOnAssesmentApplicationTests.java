735a54f8810257f86db12d36a2f817095dcb7277
dishagelani <dishagelani1999@gmail.com>
2024-10-09 22:25:04 -0400

    Initial project setup

---
+++ src/test/java/com/ThinkOn/ThinkOnAssesment/ThinkOnAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ThinkOn.ThinkOnAssesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkOnAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
